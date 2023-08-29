30d8515ee431860c17bd7d4d5ef92bb0ed201ede
hodat <hodat@gmail.com>
2023-08-29 22:45:11 +0700

    'continue'

--- src/App.js
+++ src/App.js
@@ -7,17 +7,22 @@ import ResetPasswordConfirm from './containers/ResetPasswordConfirm';
 import ResetPassword from './containers/ResetPassword';
 import Layout from './hocs/Layout';
 import Home from './containers/Home';
+import { Provider  } from 'react-redux';
+import store  from './store';
 const App = () => ( 
-    <Router>
-        <Layout>
-            <Routes>
-                
-                {/* <Route exact path='/' component={Home} /> */}
-                <Route path='/home/' element={<Home/>} />
-            </Routes>
-        </Layout>
-    </Router>
+    <Provider>
+        <Router>
+            <Layout />
+                <Routes> 
+                    <Route path='/home/' element={<Home/>} />
+                    <Route path='/login/' element={<Login/>} />
+                    <Route path='/Signup/' element={<Signup/>} />
+                    <Route path='/activate/:uid/:token/' element={<Activate/>} />
+                    <Route path='/reset-password/' element={<ResetPassword/>} />
+                    <Route path='/password/reset/confirm/:uid/:token/' element={<ResetPasswordConfirm/>} />               
+                </Routes>   
+        </Router>
+    </Provider>
 )
 
 export default App;
-
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,11 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux'
 
 const Login = () => {
-  return (
-    <div>
-      Login
-    </div>
-  )
+    const [formData, setFormData] = useState({
+      email:'',
+      password : '',
+    })
+
+    const {email, password} = formData;
+
+    const onChange = e => setFormData ({...formData,[e.target.name]: e.target.value});
+
+    const onSubmit = e => {
+      e.PreventDefault();  
+    }
+
+    return (
+      <div className='container mt-5'> 
+        <h1>Sign in</h1>
+        <p>Sign in in your account </p>
+        <form onSubmit = {e => onSubmit(e)}>
+          <div className='form-group'> 
+            <input 
+                className='form-control'
+                type='email'
+                placeholder ='Email '
+                name='email'
+                value = {email}
+                onChange = {e => onChange(e)}
+                minLength= '6'
+                required
+                />            
+          </div>
+          <div className='form-group'> 
+            <input 
+                className='form-control'
+                type='password'
+                placeholder ='Password '
+                name='password'
+                value = {password}
+                onChange = {e => onChange(e)}
+                minLength= '6'
+                required
+                />            
+          </div>
+          <button className='btn btn-primary' type='submit'>Login</button>
+        </form>
+        <p className='mt-3'>
+          Haven't account yet ? <Link to = '/Signup/'>Sign up</Link>
+        </p>
+        <p className='mt-3'>
+          Forgot password ? <Link to = '/reset-password/'>Sign up</Link>
+        </p>
+      </div>
+    )
 }
 
-export default Login;
+export default connect(null,{}) (Login);
